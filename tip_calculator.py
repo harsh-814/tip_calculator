05d1a1e8b0cb07c7c833b1e38775feb1c619eec8
harsh-814 <166392916+harsh-814@users.noreply.github.com>
2026-05-01 08:34:22 +0530

    Add files via upload

---
+++ tip_calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip Calculator!")
+bill = float(input("What was the total bill? "))
+tip = int(input("How much tip would you like to give? 10, 12, or 15 ? "))
+split = int(input("How many peoples to split the bill? "))
+pay_per_person = round((bill * (1 + tip/100))/split, 2)
+print(f"Each person should pay {pay_per_person}")
